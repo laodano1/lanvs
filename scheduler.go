5409241edba4d68d3cac8450e3b861327c231972
laodano1 <31562832+laodano1@users.noreply.github.com>
2019-03-27 10:32:07 +0800

    add task worker id to description

--- scheduler.go
+++ scheduler.go
@@ -28,29 +28,29 @@ type (
 )
 
 // task worker
-func TaskWorker(taskQ chan TaskItem, partDone chan bool, allDone chan bool)  {
+func TaskWorker(taskQ chan TaskItem, partDone chan bool, allDone chan bool, workerId int)  {
 	c := time.Tick(100 * time.Millisecond)
 	for {
 		select {
 		case oneTask := <- taskQ :
 			rand.Seed(time.Now().UnixNano())
-			i := rand.Intn(3)
+			i := rand.Intn(4)
 			// sleep
 			time.Sleep( time.Duration(i) * time.Second )
 			//fmt.Printf("random is %d second.\n", i)
-			fmt.Printf("\nstart to task '%s'\n", oneTask.Body)
+			fmt.Printf("\nstart to task '%s' in worker '%d'\n", oneTask.Body, workerId)
 			//
 			//// http request
 			//fmt.Printf("=======> do task '%s'.\n", oneTask.Body)
-			fmt.Println("do http request.")
+			fmt.Printf("do http request in task '%s' in worker '%d'.\n", oneTask.Body, workerId)
 			//
 			//// response parse
-			//fmt.Println("response parse.")
+			fmt.Printf("response parse in task '%s' in worker '%d'\n.", oneTask.Body, workerId)
 			//
 			//// inserting to DB
-			fmt.Println("inserting to db.")
+			fmt.Printf("inserting to db in task '%s' in worker '%d'\n", oneTask.Body, workerId)
 
-			fmt.Printf("------> task '%s' _done.\n", oneTask.Body)
+			fmt.Printf("------> task '%s' _done. in worker '%d' \n", oneTask.Body, workerId)
 
 			if len(taskQ) <= workerNum * 2  {
 				//time.Sleep(2 * time.Second)
@@ -62,16 +62,14 @@ func TaskWorker(taskQ chan TaskItem, partDone chan bool, allDone chan bool)  {
 			if len(taskQ) <= 0 {
 				//close(taskQ)
 				//fmt.Println("this is last task!!!! @@@@@@@@@@@")
+
 				time.Sleep(3 * time.Second)
 				allDone <- true
+				//wg.Done()
 			}
-			//if len(taskQ) <= 0 {
-			//	//wg.Done()
-			//	fmt.Println("task terminate.")
-			//	goto ter
-			//}
+
 		case <- c :
-			//time.Sleep(1 * time.Second)
+			time.Sleep(200 * time.Millisecond)
 			//fmt.Println("this is idel.---------")
 		}
 	}
@@ -93,23 +91,23 @@ func main() {
 
 	flag.IntVar(&workerNum, "wk", 5, "worker number in pool")
 	flag.IntVar(&taskNum,   "tn", 10, "task amount")
-	flag.IntVar(&repeat,   "rp", 2, "task amount")
+	flag.IntVar(&repeat,   "rp", 2, "repeat amount")
 	flag.Parse()
 
 	fmt.Printf("wokernum: %d\n", workerNum)
 	fmt.Printf("taskNum: %d\n", taskNum)
 
 	taskQ  := make(chan TaskItem, taskNum)
-
 	isPartDone := make(chan bool)
 	isAllDone  := make(chan bool)
+
 
 	// get tasks and fill task list
 	FillTaskQueue(taskQ, taskNum, "1st")
 
 	// init go routine pool
 	for i := 0; i < workerNum; i++ {
-		go TaskWorker(taskQ, isPartDone, isAllDone)
+		go TaskWorker(taskQ, isPartDone, isAllDone, i)
 	}
 
 	// infinite loop to do tasks
@@ -121,19 +119,15 @@ func main() {
 			//time.Sleep(2 * time.Second)
 			if turn < repeat {
 				fmt.Printf("----> %d tasks done. Fill task to queue in %dth time!\n", workerNum, turn + 1)
-				go FillTaskQueue(taskQ, 5, "pre" + strconv.Itoa(turn))
+				go FillTaskQueue(taskQ, 10, "added" + strconv.Itoa(turn))
 			} else {
-				//fmt.Printf("channel cap: %d/%d. Bye bye!\n", len(taskQ), workerNum * 3)
-				//wg.Wait()
-				//time.Sleep(20 * time.Second)
-				//return
-				//close(taskQ)
-				//fmt.Println("in else")
+
 			}
 			turn++
 		case <- isAllDone :
 			time.Sleep(1 * time.Second)
-			fmt.Printf("channel cap: %d/%d. Bye bye!\n", len(taskQ), workerNum * 3)
+			fmt.Printf("channel cap: %d/%d. Bye bye!\n", len(taskQ), taskNum)
+
 			return
 		}
 	}
